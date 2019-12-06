cb9fa550605c12e86ff4276a75b5616a507f8db7
dkey1 <dkey1@DESKTOP-LKSMU93>
2019-12-06 12:53:34 +0100

    init

---
+++ SlidingBlocks-DLV/src/klotski/controller/ShowMatrixController.java
@@ -0,0 +1,29 @@
+package klotski.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import klotski.model.Board;
+
+public class ShowMatrixController {
+	final Board b;
+	
+	/**
+	 * Basic constructor
+	 * @param app the view application
+	 */
+	public ShowMatrixController(Board b) {
+		this.b = b;
+	}
+	
+	/**
+	 * Print the board to textline
+	 */
+	public void show() {
+		
+	    System.out.println("R C W H\nMosse:"+b);
+	}
+}
